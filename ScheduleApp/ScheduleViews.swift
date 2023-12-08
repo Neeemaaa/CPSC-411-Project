be7989b1b6755ff75d9157341f41c33012e93bfb
Neeemaaa <89361080+Neeemaaa@users.noreply.github.com>
2023-12-08 15:32:05 -0800

    Update ScheduleViews.swift
    Moved "Add Time" and added a "Done" button that displays a message.

--- ScheduleApp/ScheduleViews.swift
+++ ScheduleApp/ScheduleViews.swift
@@ -1,92 +1,142 @@
-//
-//  ScheduleViews.swift
-//  ScheduleApp
-//
-//  Created by Madeline Sharpe on 10/24/23.
-//
-
 import SwiftUI
 
-struct AvailableTimes: View{
+struct AvailableTimes: View {
     @EnvironmentObject var manager: ScheduleManager
+    
     var body: some View {
-        VStack{
-            Text("There are currently no available times that match.")
+        VStack {
+            if manager.schedules.isEmpty {
+                Text("There are no schedules available.")
+            } else if manager.schedules.count == 1 {
+                Text("There is only one schedule available, cannot compare.")
+            } else {
+                if let commonTime = manager.findCommonTimeForTwoSchedules() {
+                    Text("Common Time: \(commonTime)")
+                        .padding()
+                } else {
+                    Text("No common time found")
+                        .padding()
+                }
+            }
         }
     }
 }
 
 struct EditableSchedulesList: View {
     @EnvironmentObject var manager: ScheduleManager
+    
     var body: some View {
         VStack {
-            // TODO: Model 3 - Add the EditButton here
             EditButton()
+            
             List {
-                /// ForEach requires each element in the collection it traverses to be Identifiable
-                ForEach(manager.schedules) {
-                                    schedule in
-                                    VStack (alignment: .leading) {
-                                        Text(schedule.person)
-                                            .font(.largeTitle)
-                                        Text(schedule.dT)
-                                            .font(.caption)
-                                    }
-                // TODO: Model 2 - Add the onDelete method below
-                                }.onDelete {
-                                    offset in
-                                    manager.schedules.remove(atOffsets: offset)
-                                }
-                // TODO: Model 3 - Add the onMove method below
-                                .onMove {
-                                    offset, index in
-                                    manager.schedules.move(fromOffsets: offset, toOffset: index)
-                                }
-
+                ForEach(manager.schedules) { schedule in
+                    VStack(alignment: .leading) {
+                        Text(schedule.person)
+                            .font(.largeTitle)
+                        Text(schedule.dT)
+                            .font(.caption)
+                    }
+                }
+                .onDelete { indices in
+                    manager.schedules.remove(atOffsets: indices)
+                }
+                .onMove { indices, newOffset in
+                    manager.schedules.move(fromOffsets: indices, toOffset: newOffset)
+                }
             }
         }
     }
 }
 
+
 struct AddDateTime: View {
-    //@SceneStorage("person") var person: String = ""
-    //@SceneStorage("dT") var dT: String = ""
     @EnvironmentObject var manager: ScheduleManager
-    @State var startTime = Date()
-    @State var endTime = Date()
-    var closedRange = Calendar.current.date(byAdding: .year, value: -1, to: Date())!
+    @State private var userName = ""
+    @State private var timeAddedMessage = "" // State variable to control the message visibility
+    @State private var startTime = Date()
+    @State private var endTime = Calendar.current.date(byAdding: .minute, value: 1, to: Date()) ?? Date()
+    @State private var shouldClearFields = false
     
     func formatDate() -> String {
-        let sComponents = Calendar.current.dateComponents([.hour, .minute, .day, .year, .month], from: startTime)
-        let sHour = sComponents.hour ?? 0
-        let sMinute = sComponents.minute ?? 0
-        let sDay = sComponents.day ?? 0
-        let sMonth = sComponents.month ?? 0
-        let sYear = sComponents.year ?? 0
+        let calendar = Calendar.current
+        let sComponents = calendar.dateComponents([.hour, .minute, .day, .month, .year], from: startTime)
+        let eComponents = calendar.dateComponents([.hour, .minute], from: endTime)
         
-        let eComponents = Calendar.current.dateComponents([.hour, .minute], from: endTime)
-        let eHour = eComponents.hour ?? 0
-        let eMinute = eComponents.minute ?? 0
-        
-        if (eHour < sHour){
+        guard let sHour = sComponents.hour,
+              let sMinute = sComponents.minute,
+              let eHour = eComponents.hour,
+              let eMinute = eComponents.minute,
+              let sDay = sComponents.day,
+              let sMonth = sComponents.month,
+              let sYear = sComponents.year else {
             return "Invalid time"
         }
-        else {
-            return "\(sMonth)/\(sDay)/\(sYear) [\(sHour):\(sMinute) - \(eHour):\(eMinute)]"
+        
+        let isValidTime = calendar.compare(startTime, to: endTime, toGranularity: .minute) == .orderedAscending
+        
+        if isValidTime {
+            let formattedStartTime = String(format: "%02d:%02d", sHour, sMinute)
+            let formattedEndTime = String(format: "%02d:%02d", eHour, eMinute)
+            
+            if startTime == endTime {
+                return "Start and end time cannot be the same"
+            } else {
+                return "\(sMonth)/\(sDay)/\(sYear) [\(formattedStartTime) - \(formattedEndTime)]"
+            }
+        } else {
+            return "Invalid time"
         }
     }
     
     var body: some View {
         Form {
-            Section(header:Text("Start Time")) {
-                DatePicker("Pick a date: ", selection: $startTime)
+            Section(header: Text("Name")){
+                TextField("Enter your name", text: $userName)
             }
-            Section(header:Text("End Time")) {
+            Section(header: Text("Please Enter Your Availability")){}
+            Section(header: Text("Start Time")) {
+                DatePicker("Pick a date/time: ", selection: $startTime)
+            }
+            Section(header: Text("End Time")) {
                 DatePicker("Pick a time: ", selection: $endTime, displayedComponents: .hourAndMinute)
             }
             Section(header: Text("Result")) {
                 Text(formatDate())
             }
+            Button("Done") {
+                let formattedTime = formatDate()
+                if !formattedTime.contains("Invalid time") && !userName.isEmpty {
+                    // Check if start time and end time are the same
+                    let sameTime = startTime == endTime
+
+                    if sameTime {
+                        timeAddedMessage = "Start and end times cannot be the same."
+                    } else {
+                        let newSchedule = Schedule(person: userName, dT: formattedTime)
+                        manager.schedules.append(newSchedule)
+                        timeAddedMessage = "Time added!"
+                        shouldClearFields = true
+                        print("Time added!")
+                    }
+                } else {
+                    timeAddedMessage = "Please enter a valid name and time."
+                }
+            }
+        }
+        .alert(isPresented: Binding<Bool>(
+            get: { !timeAddedMessage.isEmpty },
+            set: { _ in }
+        )) {
+            Alert(title: Text(timeAddedMessage), dismissButton: .default(Text("Okay")) {
+                if shouldClearFields {
+                    userName = ""
+                    startTime = Date()
+                    endTime = Calendar.current.date(byAdding: .minute, value: 1, to: Date()) ?? Date()
+                    shouldClearFields = false
+                }
+                timeAddedMessage = ""
+            })
         }
     }
 }
