2573b17ac745ecbf2ee7a8edeb0442d9f091c2a8
Neeemaaa <89361080+Neeemaaa@users.noreply.github.com>
2023-12-10 10:42:14 -0800

    Update Schedule.swift
    Properly compares 2 schedules

--- ScheduleApp/Schedule.swift
+++ ScheduleApp/Schedule.swift
@@ -4,19 +4,22 @@ class ScheduleManager: ObservableObject {
     @Published var schedules: [Schedule] = []
 
     init() {
-        schedules.append(Schedule(person: "Neema", dT: "12/08/2023 [14:00 - 15:00]"))
-        schedules.append(Schedule(person: "Bob", dT: "12/08/2023 [14:30 - 15:00]"))
+        schedules.append(Schedule(person: "Neema", dT: "12/18/2023 [14:00 - 15:00]"))
+        schedules.append(Schedule(person: "Sahar", dT: "12/18/2023 [14:30 - 15:00]"))
     }
     
     func findCommonTimeForTwoSchedules() -> String? {
         guard schedules.count == 2 else {
+            print("Incorrect number of schedules")
             return nil // Only two schedules should be compared
         }
 
         if let commonTime = findCommonTime(scheduleA: schedules[0], scheduleB: schedules[1]) {
+            print("Common Time Found: \(commonTime)")
             return commonTime
         }
 
+        print("No common time found")
         return nil // No common time found
     }
 
@@ -24,6 +27,7 @@ class ScheduleManager: ObservableObject {
     func findCommonTime(scheduleA: Schedule, scheduleB: Schedule) -> String? {
         guard let startA = extractTime(from: scheduleA.dT),
               let startB = extractTime(from: scheduleB.dT) else {
+            print("Error extracting time")
             return nil
         }
         
@@ -32,6 +36,9 @@ class ScheduleManager: ObservableObject {
         
         let commonStart = max(startA.start, startB.start)
         let commonEnd = min(startA.end, startB.end)
+        
+        print("Common Start: \(commonStart)")
+        print("Common End: \(commonEnd)")
         
         if commonStart < commonEnd {
             let dateFormatter = DateFormatter()
@@ -45,9 +52,12 @@ class ScheduleManager: ObservableObject {
             let startTime = timeFormatter.string(from: commonStart)
             let endTime = timeFormatter.string(from: commonEnd)
             
-            return "\(commonDate) [\(startTime) - \(endTime)]"
+            let commonTimeString = "\(commonDate) [\(startTime) - \(endTime)]"
+            print("Common Time String: \(commonTimeString)")
+            return commonTimeString
         }
         
+        print("No common time found")
         return nil
     }
 
@@ -56,23 +66,45 @@ class ScheduleManager: ObservableObject {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM/dd/yyyy '[HH:mm - HH:mm]'"
         
-        guard let startIndex = schedule.range(of: "[")?.upperBound,
-              let endIndex = schedule.range(of: "]")?.lowerBound else {
+        // Separate the date and time components using '[' and ']' as delimiters
+        let components = schedule.components(separatedBy: " [")
+        guard components.count == 2 else {
+            print("Error: Unexpected number of components")
             return nil
         }
         
-        let timeSlot = String(schedule[startIndex..<endIndex])
+        let dateString = components[0]
+        let timeString = components[1].replacingOccurrences(of: "]", with: "")
         
-        let timeComponents = timeSlot.components(separatedBy: " - ")
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        guard let date = dateFormatter.date(from: dateString) else {
+            print("Error: Unable to convert date string to Date")
+            return nil
+        }
+        
+        dateFormatter.dateFormat = "HH:mm"
+        let timeComponents = timeString.components(separatedBy: " - ")
         
         guard timeComponents.count == 2,
-              let startTime = dateFormatter.date(from: "\(timeComponents[0])"),
-              let endTime = dateFormatter.date(from: "\(timeComponents[1])") else {
+              let startTime = dateFormatter.date(from: timeComponents[0]),
+              let endTime = dateFormatter.date(from: timeComponents[1]) else {
+            print("Error: Unable to convert time strings to dates")
             return nil
         }
         
-        return (startTime, endTime)
+        let calendar = Calendar.current
+        let start = calendar.date(bySettingHour: calendar.component(.hour, from: startTime),
+                                  minute: calendar.component(.minute, from: startTime),
+                                  second: 0,
+                                  of: date)!
+        let end = calendar.date(bySettingHour: calendar.component(.hour, from: endTime),
+                                minute: calendar.component(.minute, from: endTime),
+                                second: 0,
+                                of: date)!
+        
+        return (start, end)
     }
+
 
     func isEarlierCommonTime(_ time1: String, than time2: String) -> Bool {
         let dateFormatter = DateFormatter()
