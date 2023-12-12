9d20ac5efef052909bba4ec52140e0ef2c38c2df
Neeemaaa <89361080+Neeemaaa@users.noreply.github.com>
2023-12-12 09:30:13 -0800

    Update ScheduleViews.swift
    Edit button now presents a message that "feature is coming soon"

--- ScheduleApp/ScheduleViews.swift
+++ ScheduleApp/ScheduleViews.swift
@@ -2,17 +2,44 @@ import SwiftUI
 
 struct AvailableTimes: View {
     @EnvironmentObject var manager: ScheduleManager
-    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM dd, yyyy"
+        return formatter
+    }()
+
     var body: some View {
         VStack {
             if manager.schedules.isEmpty {
-                Text("There are no schedules available.")
+                Text("There are no schedules available.").multilineTextAlignment(.center)
             } else if manager.schedules.count == 1 {
-                Text("There is only one schedule available, cannot compare.")
+                Text("There is only one schedule available, cannot compare to anything.").multilineTextAlignment(.center)
+            } else if manager.schedules.count >= 3 {
+                Text("This application can only compare 2 schedules, please remove \(manager.schedules.count-2) of the schedules.")
+                .multilineTextAlignment(.center)
             } else {
                 if let commonTime = manager.findCommonTimeForTwoSchedules() {
-                    Text("Common Time: \(commonTime)")
-                        .padding()
+                    let components = commonTime.components(separatedBy: " ")
+                    VStack(alignment: .center, spacing: 8) {
+                        Text("Common Time:")
+                            .font(.headline)
+                            .padding(.bottom, 4)
+                        let people = manager.schedules.map { $0.person }.joined(separator: " and ")
+                                            Text("\(people) are both available at")
+                                                .font(.headline)
+                                                .padding(.bottom, 4)
+                        Text("\(formattedDate(from: components[0]))") // Date
+                            .font(.system(size: 18, weight: .bold))
+                            .padding(.bottom, 4)
+                        
+                        Text("\(components[1]) \(components[2]) \(components[3])") // Time Range
+                            .font(.system(size: 16))
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 8)
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(8)
+                    }
+                    .padding()
                 } else {
                     Text("No common time found")
                         .padding()
@@ -20,30 +47,65 @@ struct AvailableTimes: View {
             }
         }
     }
+    func formattedDate(from dateString: String) -> String {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "MM/dd/yyyy"
+
+        if let date = inputFormatter.date(from: dateString) {
+            let outputFormatter = DateFormatter()
+            outputFormatter.dateFormat = "*MMMM dd, yyyy*"
+            return outputFormatter.string(from: date)
+        } else {
+            return "Invalid Date"
+        }
+    }
 }
+
 
 struct EditableSchedulesList: View {
     @EnvironmentObject var manager: ScheduleManager
+    @State private var isShowingAlert = false
     
     var body: some View {
-        VStack {
-            EditButton()
-            
-            List {
-                ForEach(manager.schedules) { schedule in
-                    VStack(alignment: .leading) {
-                        Text(schedule.person)
-                            .font(.largeTitle)
-                        Text(schedule.dT)
-                            .font(.caption)
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(manager.schedules) { schedule in
+                        VStack(alignment: .leading) {
+                            Text(schedule.person)
+                                .font(.largeTitle)
+                                .onTapGesture {
+                                    if !isShowingAlert {
+                                        isShowingAlert.toggle()
+                                    }
+                                }
+                            Text(schedule.dT)
+                                .font(.caption)
+                        }
+                    }
+                    .onDelete { indices in
+                        manager.schedules.remove(atOffsets: indices)
+                    }
+                    .onMove { indices, newOffset in
+                        manager.schedules.move(fromOffsets: indices, toOffset: newOffset)
                     }
                 }
-                .onDelete { indices in
-                    manager.schedules.remove(atOffsets: indices)
+                .navigationBarTitle("Schedules")
+                .navigationBarItems(trailing:
+                    Button(action: {
+                        isShowingAlert.toggle()
+                    }) {
+                        Text("Edit")
+                    }
+                )
+                .alert(isPresented: $isShowingAlert) {
+                    Alert(title: Text("Editing Feature To Be Added Later, Swipe Name To The Left To Delete"))
                 }
-                .onMove { indices, newOffset in
-                    manager.schedules.move(fromOffsets: indices, toOffset: newOffset)
-                }
+                
+                Text("Current Schedule Count: \(manager.schedules.count)")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color.gray)
+                    .padding(.bottom, 20)
             }
         }
     }
@@ -55,7 +117,7 @@ struct AddDateTime: View {
     @State private var userName = ""
     @State private var timeAddedMessage = "" // State variable to control the message visibility
     @State private var startTime = Date()
-    @State private var endTime = Calendar.current.date(byAdding: .minute, value: 1, to: Date()) ?? Date()
+    @State private var endTime = Date() // Default value for endTime
     @State private var shouldClearFields = false
     
     func formatDate() -> String {
@@ -79,7 +141,7 @@ struct AddDateTime: View {
             let formattedStartTime = String(format: "%02d:%02d", sHour, sMinute)
             let formattedEndTime = String(format: "%02d:%02d", eHour, eMinute)
             
-            if startTime == endTime {
+            if formattedStartTime == formattedEndTime {
                 return "Start and end time cannot be the same"
             } else {
                 return "\(sMonth)/\(sDay)/\(sYear) [\(formattedStartTime) - \(formattedEndTime)]"
@@ -91,25 +153,37 @@ struct AddDateTime: View {
     
     var body: some View {
         Form {
-            Section(header: Text("Name")){
+            Section(header: Text("Name")) {
                 TextField("Enter your name", text: $userName)
             }
-            Section(header: Text("Please Enter Your Availability")){}
+            
+            Section(header: Text("Please Enter Your Availability")) {}
+            
             Section(header: Text("Start Time")) {
                 DatePicker("Pick a date/time: ", selection: $startTime)
             }
+            
             Section(header: Text("End Time")) {
                 DatePicker("Pick a time: ", selection: $endTime, displayedComponents: .hourAndMinute)
+                    .onChange(of: endTime, perform: { value in
+                        let calendar = Calendar.current
+                        let components = calendar.dateComponents([.hour, .minute], from: value)
+                        if let hour = components.hour, let minute = components.minute {
+                            let newEndTime = calendar.date(bySettingHour: hour, minute: minute, second: 0, of: startTime)
+                            endTime = newEndTime ?? value
+                        }
+                    })
             }
+            
             Section(header: Text("Result")) {
                 Text(formatDate())
             }
+            
             Button("Done") {
                 let formattedTime = formatDate()
                 if !formattedTime.contains("Invalid time") && !userName.isEmpty {
-                    // Check if start time and end time are the same
                     let sameTime = startTime == endTime
-
+                    
                     if sameTime {
                         timeAddedMessage = "Start and end times cannot be the same."
                     } else {
@@ -124,6 +198,9 @@ struct AddDateTime: View {
                 }
             }
         }
+        .onChange(of: startTime) { newValue in
+            endTime = Calendar.current.date(byAdding: .minute, value: 1, to: newValue) ?? Date()
+        }
         .alert(isPresented: Binding<Bool>(
             get: { !timeAddedMessage.isEmpty },
             set: { _ in }
@@ -132,7 +209,7 @@ struct AddDateTime: View {
                 if shouldClearFields {
                     userName = ""
                     startTime = Date()
-                    endTime = Calendar.current.date(byAdding: .minute, value: 1, to: Date()) ?? Date()
+                    endTime = Date()
                     shouldClearFields = false
                 }
                 timeAddedMessage = ""
